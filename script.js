1405ac78e0fe5bb10fcf35f05669f8e978bf5ef7
Domenicopezzullo <pezzullodomenico712@gmail.com>
2024-04-26 22:16:35 +0200

    skibi

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     var faqsSection = document.getElementById('faqs');
-    
+    var sidebar = document.getElementById("sidebar")
 
     document.querySelector('nav ul li a').addEventListener('click', function (event) {
         event.preventDefault();
         faqsSection.style.display = faqsSection.style.display === 'none' ? 'block' : 'none';
+
     });
+
+	function toggleSidebar() {
+		if (sidebar.style.left === "0px") {
+			sidebar.style.left = "-250px";
+		} else {
+			sidebar.style.left = "0px";
+		}
+	}
 });
 
 
 
-
-var sidebar = document.getElementById("sidebar");
-function toggleSidebar() {
-    if (sidebar.style.left === "0px") {
-        sidebar.style.left = "-250px";
-    } else {
-        sidebar.style.left = "0px";
-    }
-}
